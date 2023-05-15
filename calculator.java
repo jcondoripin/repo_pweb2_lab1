876dc01a36e28855fe9318c42e36d7a5cf0756f7
jcondoripin <jcondoripin@unsa.edu.pe>
2023-05-15 11:50:10 -0500

    calculator class

---
+++ calculator.java
@@ -0,0 +1,20 @@
+public class calculator {
+	public static void main(String[] args) {
+		// Here the method test
+	}
+	public static int add(int a, int b) {
+
+	}
+	public static int sub(int a, int b) {
+
+	}
+	public static int mul(int a, int b) {
+
+	}
+	public static int div(int a, int b) {
+
+	}
+	public static int mod(int a, int b) {
+
+	}
+}
