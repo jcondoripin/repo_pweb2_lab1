eb8802885b6915655beead0decef4375661b820a
jcondoripin <jcondoripin@unsa.edu.pe>
2023-05-15 23:56:04 -0500

    calculator class test

--- calculator.java
+++ calculator.java
@@ -1,12 +1,18 @@
 public class calculator {
 	public static void main(String[] args) {
-		// Here the method test
+		int a = 5;
+		int b = 10;
+		System.out.println(add(a,b));
+		System.out.println(sub(a,b));
+		System.out.println(mul(a,b));
+		System.out.println(div(a,b));
+		System.out.println(mod(a,b));
 	}
 	public static int add(int a, int b) {
 		return a+b;
 	}
 	public static int sub(int a, int b) {
-                return a-b;
+        return a-b;
 	}
 	public static int mul(int a, int b) {
 		return a*b;
